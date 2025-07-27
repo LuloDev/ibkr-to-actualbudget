566f8d68f1502596c5e97bad8768c6bd26167240
LuloDev <134250506+LuloDev@users.noreply.github.com>
2025-07-27 17:07:31 -0500

    Fix: Directly inject environment variables into crontab for cron jobs

--- cron_entrypoint.sh
+++ cron_entrypoint.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-export NODE_ENV=$NODE_ENV
-export IBKR_TOKEN=$IBKR_TOKEN
-export IBKR_QUERY_ID=$IBKR_QUERY_ID
-export ACTUAL_BUDGET_ID=$ACTUAL_BUDGET_ID
-export ACTUAL_SYNC_ACCOUNT_ID=$ACTUAL_SYNC_ACCOUNT_ID
-export ACTUAL_SERVER_URL=$ACTUAL_SERVER_URL
-export ACTUAL_SERVER_PASSWORD=$ACTUAL_SERVER_PASSWORD
-export ACTUAL_BUDGET_CURRENCY=$ACTUAL_BUDGET_CURRENCY
-export EXCHANGE_RATE_API_KEY=$EXCHANGE_RATE_API_KEY
-
 # Default cron schedule
 CRON_SCHEDULE=${CRON_SCHEDULE:-'55 23 * * *'}
 
 # Create a temporary crontab file
-echo "MAILTO=" > /etc/crontabs/root
+echo "MAILTO=\"\"" > /etc/crontabs/root
+echo "NODE_ENV=\"$NODE_ENV\"" >> /etc/crontabs/root
+echo "IBKR_TOKEN=\"$IBKR_TOKEN\"" >> /etc/crontabs/root
+echo "IBKR_QUERY_ID=\"$IBKR_QUERY_ID\"" >> /etc/crontabs/root
+echo "ACTUAL_BUDGET_ID=\"$ACTUAL_BUDGET_ID\"" >> /etc/crontabs/root
+echo "ACTUAL_SYNC_ACCOUNT_ID=\"$ACTUAL_SYNC_ACCOUNT_ID\"" >> /etc/crontabs/root
+echo "ACTUAL_SERVER_URL=\"$ACTUAL_SERVER_URL\"" >> /etc/crontabs/root
+echo "ACTUAL_SERVER_PASSWORD=\"$ACTUAL_SERVER_PASSWORD\"" >> /etc/crontabs/root
+echo "ACTUAL_BUDGET_CURRENCY=\"$ACTUAL_BUDGET_CURRENCY\"" >> /etc/crontabs/root
+echo "EXCHANGE_RATE_API_KEY=\"$EXCHANGE_RATE_API_KEY\"" >> /etc/crontabs/root
 echo "$CRON_SCHEDULE /usr/local/bin/node /usr/src/app/dist/index.js 2>&1 | tee -a /var/log/cron.log" >> /etc/crontabs/root
 
 # Give execute permissions to the crontab file

cf783ee90b14ba90decaff86097e1da12219827a
LuloDev <134250506+LuloDev@users.noreply.github.com>
2025-07-27 16:44:54 -0500

    Fix: Node not found in cron and display logs in console

--- cron_entrypoint.sh
+++ cron_entrypoint.sh
@@ -5,10 +5,11 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-'55 23 * * *'}
 
 # Create a temporary crontab file
 echo "MAILTO=" > /etc/crontabs/root
-echo "$CRON_SCHEDULE node /usr/src/app/dist/index.js 2>&1 | tee -a /var/log/cron.log" >> /etc/crontabs/root
+echo "$CRON_SCHEDULE /usr/local/bin/node /usr/src/app/dist/index.js 2>&1 | tee -a /var/log/cron.log" >> /etc/crontabs/root
 
 # Give execute permissions to the crontab file
 chmod 0644 /etc/crontabs/root
 
 # Start cron in the foreground
-crond -f
+crond
+tail -f /var/log/cron.log
